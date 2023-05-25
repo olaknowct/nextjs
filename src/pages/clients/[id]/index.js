6791ba04b8a852fec46d996e504de5a356025cc2
Christopher Olano <olanochristopher@gmail.com>
2023-05-25 08:57:26 +0800

    Nested dynamic routes

---
+++ src/pages/clients/[id]/index.js
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+function ClientProjectsPage() {
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button>Load Project A</button>
+    </div>
+  );
+}
+
+export default ClientProjectsPage;
