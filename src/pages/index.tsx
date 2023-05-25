64acb7d7f1695273a723cb75d2c6e53b9f7bc8fd
Christopher Olano <olanochristopher@gmail.com>
2023-05-25 09:17:17 +0800

    Navigating using Link component from next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1>The home Page</h1>
+    <main className={`flex min-h-screen flex-col items-center  ${inter.className}`}>
+      <h1 className='text-5xl'>The home Page</h1>
+      <ul>
+        <li>
+          <Link href='/portfolio' className='underline'>
+            Porfolio
+          </Link>
+        </li>
+        <li>
+          <Link href='/clients' className='underline'>
+            Clients
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
