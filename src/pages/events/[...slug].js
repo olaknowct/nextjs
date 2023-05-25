65443a180c2024d8dd3d12a73c027ab7e4c389e7
Christopher Olano <olanochristopher@gmail.com>
2023-05-25 22:12:29 +0800

    Showing filtered events

--- src/pages/events/[...slug].js
+++ src/pages/events/[...slug].js
@@ -1,7 +1,70 @@
-export default function FilteredEvents() {
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../../events-model';
+import EventList from '../../components/events/event-list';
+import ResultsTitle from '../../components/events/results-title';
+import Button from '../../components/ui/button';
+import ErrorAlert from '../../components/ui/error-alert';
+
+function FilteredEvents() {
+  let errorMsg;
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  const isInvalidInputs =
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12;
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  const date = new Date(numYear, numMonth - 1);
+
+  const noEvents = !filteredEvents || filteredEvents.length === 0;
+
+  const hasError = isInvalidInputs || noEvents;
+
+  if (noEvents) errorMsg = 'No events found for the chosen filter!';
+
+  if (isInvalidInputs) errorMsg = 'Invalid filter. Please adjust your values!';
+
   return (
-    <div>
-      <h1>Filtered Events</h1>
-    </div>
+    <Fragment>
+      {hasError ? (
+        <>
+          <ErrorAlert>
+            <p>{errorMsg}</p>
+          </ErrorAlert>
+          <div className='center'>
+            <Button link='/events'>Show All Events</Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <ResultsTitle date={date} />
+          <EventList events={filteredEvents} />
+        </>
+      )}
+    </Fragment>
   );
 }
+
+export default FilteredEvents;
