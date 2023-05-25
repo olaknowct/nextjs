6685fba7105b1222628474981b17becddd0ea5f3
Christopher Olano <olanochristopher@gmail.com>
2023-05-25 20:43:10 +0800

    Find events using useRef and router push

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,11 +1,23 @@
 import { getAllEvents } from '../../../events-model';
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+import { useRouter } from 'next/router';
 
 export default function Events() {
+  const router = useRouter();
+
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
   return (
-    <div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
-    </div>
+    </>
   );
 }
