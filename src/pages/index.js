badbb5c608bfa2be9b936f557611f9b4a835382a
Christopher Olano <olanochristopher@gmail.com>
2023-05-28 11:33:58 +0800

    running server side code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+// next js will ignore it during the build since this is part of the getStaticprops
+import fs from 'fs/promises';
+import path from 'path';
+
 export default function Home(props) {
   const { products } = props;
   return (
@@ -11,10 +15,14 @@ export default function Home(props) {
 
 // code that will never be visible on the client side
 // nextjs will run this code before running the function component
+// any code inside wil get executed in the serverside
 export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
   return {
     props: {
-      products: [{ id: 'p1', title: 'Product test' }],
+      products: data.products,
     },
   };
 }
