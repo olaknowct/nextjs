ea89eb95bfafec4d4c27c188840608923f24bbbd
Christopher Olano <olanochristopher@gmail.com>
2023-06-25 02:26:53 +0800

    static site generation(SSG) on the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
-import { getFeaturedEvents } from '../../events-model';
+// import { getFeaturedEvents } from '../../events-model';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+export default function Home({ events }: any) {
+  // we can use useEffect to fetch
   return (
     <div>
-      <EventList events={featuredEvents} />
+      <EventList events={events} />
     </div>
   );
+}
+
+// it make sense to pre render it for SEO since its a home page or root rather
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
