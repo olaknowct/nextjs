b0a76158721697c8a138ef825046e32e4de3aeef
Christopher Olano <olanochristopher@gmail.com>
2023-06-28 00:27:46 +0800

    SSG: Pre-render with ISG (revalidate)

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,12 +1,13 @@
-import { getAllEvents } from '../../../events-model';
-import EventList from '../../components/events/event-list';
-import EventsSearch from '../../components/events/events-search';
+import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-export default function Events() {
-  const router = useRouter();
+import { getAllEvents } from '../../helpers/api-util';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
-  const events = getAllEvents();
+function AllEventsPage(props) {
+  const router = useRouter();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
     const fullPath = `/events/${year}/${month}`;
@@ -15,9 +16,22 @@ export default function Events() {
   }
 
   return (
-    <>
+    <Fragment>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList events={events} />
-    </>
+    </Fragment>
   );
 }
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
+
+export default AllEventsPage;
