64bed75559ebd64349b879f92e82c3f59077a393
Christopher Olano <olanochristopher@gmail.com>
2023-07-02 00:24:07 +0800

    optimizing image using image from next

--- src/components/events/event-item.jsx
+++ src/components/events/event-item.jsx
@@ -1,10 +1,12 @@
+import Image from 'next/image';
+
 import classes from './event-item.module.css';
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 
-const EventItem = (props) => {
+function EventItem(props) {
   const { title, image, date, location, id } = props;
 
   const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
@@ -12,16 +14,15 @@ const EventItem = (props) => {
     month: 'long',
     year: 'numeric',
   });
-
   const formattedAddress = location.replace(', ', '\n');
   const exploreLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt=''></img>
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={classes.content}>
         <div className={classes.summary}>
-          <h2>Title</h2>
+          <h2>{title}</h2>
           <div className={classes.date}>
             <DateIcon />
             <time>{humanReadableDate}</time>
@@ -42,6 +43,6 @@ const EventItem = (props) => {
       </div>
     </li>
   );
-};
+}
 
 export default EventItem;
