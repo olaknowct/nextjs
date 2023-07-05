6a2d7a61e1943e174864d219460b5f4f824da29f
Christopher Olano <olanochristopher@gmail.com>
2023-07-05 13:48:41 +0800

    news letter sign up via /api/newsletter POST

--- src/components/input/newsletter-registration.js
+++ src/components/input/newsletter-registration.js
@@ -1,12 +1,24 @@
+import { useRef } from 'react';
+
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
+  const emailInputRef = useRef();
+
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
-    // optional: validate input
-    // send valid data to API
+    const enteredEmail = emailInputRef.current.value;
+
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   }
 
   return (
@@ -14,7 +26,13 @@ function NewsletterRegistration() {
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
-          <input type='email' id='email' placeholder='Your email' aria-label='Your email' />
+          <input
+            type='email'
+            id='email'
+            placeholder='Your email'
+            aria-label='Your email'
+            ref={emailInputRef}
+          />
           <button>Register</button>
         </div>
       </form>
