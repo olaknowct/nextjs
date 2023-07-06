c446ba8f7d430d2573b6a7dd8abf2103d33efdf0
Christopher Olano <olanochristopher@gmail.com>
2023-07-06 23:41:19 +0800

    added notification context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,21 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout/layout';
 import Head from 'next/head';
+import { NotificationContextProvider } from '../store/notification-context';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      {/* Generic Head that applies to every single page rendered */}
-      {/* Content will be merege if there are multiple Head and resolves conflicts, simply takes the latest elements */}
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        {/* Generic Head that applies to every single page rendered */}
+        {/* Content will be merege if there are multiple Head and resolves conflicts, simply takes the latest elements */}
+        <Head>
+          <title>Next Events</title>
+          <meta name='description' content='NextJS Events' />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
