912062e368286541e83987faa5afd2a3c4319daf
Christopher Olano <olanochristopher@gmail.com>
2023-07-11 18:35:17 +0800

    add hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
-import { Fragment } from 'react';
+import Image from 'next/image';
 
-function HomePage() {
+import classes from './hero.module.css';
+
+function Hero() {
   return (
-    <Fragment>
-      <Hero />
-      <FeaturedPosts />
-    </Fragment>
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image src='/images/site/max.png' alt='An image showing Max' width={300} height={300} />
+      </div>
+      <h1>Hi, I'm Max</h1>
+      <p>I blog about web development - especially frontend frameworks like Angular or React.</p>
+    </section>
   );
 }
 
-export default HomePage;
+export default Hero;
