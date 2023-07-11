da962297968c81d28aa21df92f16bac585c6b0d5
Christopher Olano <olanochristopher@gmail.com>
2023-07-11 22:56:10 +0800

    blog complete

--- src/components/home-page/hero.js
+++ src/components/home-page/hero.js
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className={classes.hero}>
       <div className={classes.image}>
-        <Image src='/images/site/max.png' alt='An image showing Max' width={300} height={300} />
+        <Image src='/images/site/max.jpg' alt='An image showing Max' width={300} height={300} />
       </div>
       <h1>Hi, I'm Max</h1>
       <p>I blog about web development - especially frontend frameworks like Angular or React.</p>
--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,3 +1,50 @@
-function PostDetailPage() {}
+import Head from 'next/head';
+import { Fragment } from 'react';
+
+import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
+import { useRouter } from 'next/router';
+
+function PostDetailPage(props) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Fragment>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta name='description' content={props.post.excerpt} />
+      </Head>
+      <PostContent post={props.post} />
+    </Fragment>
+  );
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 10,
+  };
+}
+
+export function getStaticPaths() {
+  const postFilenames = getPostsFiles();
+
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    // paths: [{ params: { slug: 'getting-started-with-nextjs' } }],
+    fallback: true,
+  };
+}
 
 export default PostDetailPage;
